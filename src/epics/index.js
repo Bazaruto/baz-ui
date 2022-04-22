2423ad8e1a40ad0a384df35bb0780e6660eb5dd3
Markus Coetzee <mks.coetzee@gmail.com>
2022-04-22 09:11:29 +0200

    Remove redux utils

--- src/epics/index.js
+++
@@ -1,4 +0,0 @@
-export { ofType, flatAjax, switchAjax, pluckPayload } from './custom-operators';
-export { ajaxEpic, switchAjaxEpic, debouncedAjaxEpic } from './ajax';
-
-export const is = (action, type) => action.type === type;
