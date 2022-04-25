f4ebe6c8a5e9d0e1a98564f08487572c7ce1b542
Markus Coetzee <mks.coetzee@gmail.com>
2022-04-25 14:59:43 +0200

    Experimental: Use Typescript

--- src/utils/collection-utils.ts
+++ src/utils/collection-utils.ts
@@ -1,25 +1,31 @@
 import {EMPTY_ARRAY} from '../constants';
 
-export function findWithMatchingFields(collection, { fieldToMatch }, query='') {
+export function findWithMatchingFields<T extends Record<string, unknown>>(
+  collection: T[],
+  { fieldToMatch }: { fieldToMatch: keyof T },
+  query = ''
+) {
   if (query.length < 2) {
     return EMPTY_ARRAY;
   }
 
-  const searchRegexString = query.trim()
+  const searchRegexString = query
+    .trim()
     .split(' ')
     // Only use terms longer than 1 char
     .filter(term => term.trim().length > 1)
     // Sanitize special chars
     .map(term => term.replace(/[.?*+^$[\]\\(){}|-]/g, '\\$&'))
     // Search ahead when multiple terms provided
-    .join('(\\S)*(\\s)+(.)*')
+    .join('(\\S)*(\\s)+(.)*');
 
   const searchRegex = new RegExp(searchRegexString, 'i');
 
   const matches = [];
   for (let i = 0; i < collection.length; i++) {
     const obj = collection[i];
-    if (searchRegex.test(obj[fieldToMatch])) {
+    const field = obj[fieldToMatch] as string;
+    if (searchRegex.test(field)) {
       matches.push(obj);
     }
   }
