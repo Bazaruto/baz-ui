1bd336be0a9524b33b7a4027f86a8e4af3942b55
Kristen-baz <kristen@bazaruto.com>
2024-11-12 17:10:26 +0200

    Allow for not showing datepicker side by side

--- src/components/DateRange.js
+++ src/components/DateRange.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Moment from 'moment';
-import { formatDateTime, formatDate } from '../utils/date-utils';
-import { openAtDate } from './InputDatePicker';
+import React from "react";
+import PropTypes from "prop-types";
+import Moment from "moment";
+import { formatDateTime, formatDate } from "../utils/date-utils";
+import { openAtDate } from "./InputDatePicker";
 
 export default class DateRange extends React.Component {
   static propTypes = {
@@ -16,12 +16,13 @@ export default class DateRange extends React.Component {
     defaultDays: PropTypes.number,
     monthLimit: PropTypes.number,
     showTimePicker: PropTypes.bool,
+    sideBySide: PropTypes.bool,
   };
 
   static defaultProps = {
     defaultDays: 0,
-    startDateName: 'startDate',
-    endDateName: 'endDate',
+    startDateName: "startDate",
+    endDateName: "endDate",
   };
 
   state = {
@@ -33,8 +34,12 @@ export default class DateRange extends React.Component {
     return this.props.showTimePicker ? formatDateTime(date) : formatDate(date);
   }
 
-  setStartDateInputRef = input => { this.startDateInput = input; };
-  setEndDateInputRef = input => { this.endDateInput = input; };
+  setStartDateInputRef = (input) => {
+    this.startDateInput = input;
+  };
+  setEndDateInputRef = (input) => {
+    this.endDateInput = input;
+  };
 
   initWithStartDate(startDate) {
     // Open end date picker up at the same place as this start date
@@ -50,10 +55,15 @@ export default class DateRange extends React.Component {
     this.props.onChange({ [this.props.endDateName]: endDate }, true);
   }
 
-  handleDateRangeChange = newState => {
-    const { startDateName, endDateName, defaultDays, monthLimit, minDate } = this.props;
-    let startDate = newState[startDateName] || this.props.startDate || this.state.initStartDate;
-    let endDate = newState[endDateName] || this.props.endDate || this.state.initEndDate;
+  handleDateRangeChange = (newState) => {
+    const { startDateName, endDateName, defaultDays, monthLimit, minDate } =
+      this.props;
+    let startDate =
+      newState[startDateName] ||
+      this.props.startDate ||
+      this.state.initStartDate;
+    let endDate =
+      newState[endDateName] || this.props.endDate || this.state.initEndDate;
 
     if (!endDate) {
       // Busy initialising from blank range, we have only picked a start date at this point
@@ -73,19 +83,19 @@ export default class DateRange extends React.Component {
 
     if (startChanged) {
       if (start.isAfter(end)) {
-        end = Moment(start).add(defaultDays, 'days');
+        end = Moment(start).add(defaultDays, "days");
       }
       if (monthLimit) {
-        end = Moment.min(end, Moment(start).add(monthLimit, 'months')); // Limited to a window
+        end = Moment.min(end, Moment(start).add(monthLimit, "months")); // Limited to a window
       }
     }
 
     if (!startChanged) {
       if (end.isBefore(start)) {
-        start = Moment(end).subtract(defaultDays, 'days');
+        start = Moment(end).subtract(defaultDays, "days");
       }
       if (monthLimit) {
-        start = Moment.max(start, Moment(end).subtract(monthLimit, 'months')); // Limited to a window
+        start = Moment.max(start, Moment(end).subtract(monthLimit, "months")); // Limited to a window
       }
       if (minDate) {
         start = Moment.max(start, Moment(minDate));
@@ -94,7 +104,7 @@ export default class DateRange extends React.Component {
 
     this.props.onChange({
       [startDateName]: this.format(start),
-      [endDateName]: this.format(end)
+      [endDateName]: this.format(end),
     });
 
     // We no longer need this temporary state
@@ -104,22 +114,25 @@ export default class DateRange extends React.Component {
   };
 
   render() {
-    return this.props.children({
+    return this.props.children(
+      {
         onChange: this.handleDateRangeChange,
         value: this.props.startDate,
         name: this.props.startDateName,
         minValue: this.props.minDate,
         inputRef: this.setStartDateInputRef,
         showTimePicker: this.props.showTimePicker,
-      }, {
+        sideBySide: this.props.sideBySide,
+      },
+      {
         onChange: this.handleDateRangeChange,
         value: this.props.endDate,
         name: this.props.endDateName,
         minValue: this.props.minDate,
         inputRef: this.setEndDateInputRef,
         showTimePicker: this.props.showTimePicker,
+        sideBySide: this.props.sideBySide,
       }
     );
   }
 }
-
--- src/components/DateTimeInput.js
+++ src/components/DateTimeInput.js
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import Inputable from './Inputable';
-import InputDatePicker from './InputDatePicker';
+import PropTypes from "prop-types";
+import React from "react";
+import Inputable from "./Inputable";
+import InputDatePicker from "./InputDatePicker";
 
 const propTypes = {
   id: PropTypes.string,
@@ -10,10 +10,7 @@ const propTypes = {
   minValue: PropTypes.string,
   maxValue: PropTypes.string,
   name: PropTypes.string,
-  label: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.element,
-  ]),
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
   showMessage: PropTypes.bool,
   message: PropTypes.string,
   style: PropTypes.object,
@@ -21,17 +18,27 @@ const propTypes = {
   disabled: PropTypes.bool,
 
   showTimePicker: PropTypes.bool,
+  sideBySide: PropTypes.bool,
   viewMode: PropTypes.string,
   clearable: PropTypes.bool,
-  onClear: PropTypes.func
+  onClear: PropTypes.func,
 };
 
 const defaultProps = {
-  value: ''
+  value: "",
 };
 
-let DateTimeInput = ({ onChange, value, minValue, maxValue, name, clearable, onClear, ...rest }) => (
-  <div className={`input-date-picker ${clearable ? 'input-group' : ''}`}>
+let DateTimeInput = ({
+  onChange,
+  value,
+  minValue,
+  maxValue,
+  name,
+  clearable,
+  onClear,
+  ...rest
+}) => (
+  <div className={`input-date-picker ${clearable ? "input-group" : ""}`}>
     <InputDatePicker
       identifier={name}
       startDate={value}
@@ -40,13 +47,13 @@ let DateTimeInput = ({ onChange, value, minValue, maxValue, name, clearable, onC
       onApply={onChange}
       {...rest}
     />
-    {clearable &&
+    {clearable && (
       <span className="input-group-btn">
         <button type="button" className="btn btn-default" onClick={onClear}>
           <span className="glyphicon glyphicon-remove"></span>
         </button>
       </span>
-    }
+    )}
   </div>
 );
 
@@ -54,4 +61,3 @@ DateTimeInput = Inputable(DateTimeInput);
 DateTimeInput.propTypes = propTypes;
 DateTimeInput.defaultProps = defaultProps;
 export default DateTimeInput;
-
